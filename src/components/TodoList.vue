f7e3197e20792546c82a4c8f3daf412e632bfb76
Javier Rojas <javiereduardorh@gmail.com>
2019-08-06 12:20:10 -0400

    Bottons added

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,10 +3,17 @@
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
-            <h1 font-hairline text-center text-gray-500>Lista de tareas</h1>
+            <h1 class="font-hairline text-center text-gray-500">Lista de tareas</h1>
         </todo-card>
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
-            Create/Edit Task
+            <div class="text-center">
+                <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Submit
+                </button>    
+                <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ">
+                    Erase
+                </button>                   
+            </div>
         </todo-card>
     </div>
 </template>
