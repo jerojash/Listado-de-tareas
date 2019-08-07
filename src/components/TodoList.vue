246bc6280f209963f9096bd7804da70d9b450768
Christian <cguette@typeiqs.com>
2019-08-07 09:33:57 -0400

    Agregar casillas de enmarcado

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
+        <h1 class="font-hairline text-6xl text-center text-gray-500">List of tasks</h1>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
-            <h1 class="font-hairline text-center text-gray-500">Lista de tareas</h1>
+           
+            <h1 class="font-hairline text-center text-gray-500">Tasks</h1>
+            <div class="flex mb-4">
+             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
+                 <input type="checkbox" name="terms" onclick="disableSending()">
+             </div>
+             </div><div class="flex mb-4">
+             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
+                 <input type="checkbox" name="terms" onclick="disableSending()">
+             </div>
+             </div>
+             <div class="flex mb-4">
+             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
+                 <input type="checkbox" name="terms" onclick="disableSending()">
+             </div>
+             </div>
+
         </todo-card>
+        
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             <div class="text-center">
                 <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Escribir tarea" aria-label="Full name">
@@ -14,7 +31,7 @@
             </div>
         </todo-card>
     </div>
-</template>
+  </template>
 
 <script>
 export default {
