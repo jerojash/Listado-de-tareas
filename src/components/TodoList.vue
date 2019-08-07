92b1e9caaf9d0c39531f1671f79cb8c432d45368
Javier Rojas <javiereduardorh@gmail.com>
2019-08-07 11:08:07 -0400

    Input hoover

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
+        <h1 class="font-hairline text-6xl text-center text-gray-500">Tareas por hacer</h1>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
             <h1 class="font-hairline text-center text-gray-500">Lista de tareas</h1>
         </todo-card>
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             <div class="text-center">
-                <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Escribir tarea" aria-label="Full name">
+                <input class=" hover:bg-gray-200 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Escribir tarea" aria-label="Full name">
                 <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                     Submit
                 </button>                    
