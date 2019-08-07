d96d21e9e08f61c79fa7ab210d23ce61cf57c0af
Christian <cguette@typeiqs.com>
2019-08-07 11:16:23 -0400

    cambios de listado

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,37 +1,34 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">List of tasks</h1>
+        <h1 class="font-hairline text-6xl text-center text-gray-500">List of Task</h1>
 
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
-           
-            <h1 class="font-hairline text-center text-gray-500">Tasks</h1>
+            <h1 class="font-hairline text-center text-gray-500">Task</h1>
             <div class="flex mb-4">
-             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
-                 <input type="checkbox" name="terms" onclick="disableSending()">
-             </div>
-             </div><div class="flex mb-4">
-             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
-                 <input type="checkbox" name="terms" onclick="disableSending()">
-             </div>
-             </div>
-             <div class="flex mb-4">
-             <div class="w-4/3 bg-gray-400 h-8">Realizar componentes
-                 <input type="checkbox" name="terms" onclick="disableSending()">
-             </div>
-             </div>
-
+            <div class="w-3/2 bg-gray-500 h-12">
+                <h1>Primera Tarea
+                <button class="  bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Delete
+                </button>
+                <div class="text-right"></div>
+                <button class=" bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    Edit
+                </button>
+                    <input type="checkbox" name="" id="">
+                </h1>
+            </div>
+            </div>
         </todo-card>
-        
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             <div class="text-center">
                 <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Escribir tarea" aria-label="Full name">
-                <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                <button class=" text-left bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                     Submit
                 </button>                    
             </div>
         </todo-card>
     </div>
-  </template>
+</template>
 
 <script>
 export default {
